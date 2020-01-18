6bfec224a39bef632ad420f0bc68198d8888fd00
fairouz <fairouzkoehlerwest@gmail.com>
2020-01-18 22:39:24 +0100

    Connects backend

--- app.js
+++ app.js
@@ -39,6 +39,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use("/", indexRouter);
 // app.use("/users", usersRouter);
 
@@ -49,14 +55,14 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+// app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  // res.locals.message = err.message;
+  // res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
+  // res.status(err.status || 500);
+  // res.render("error");
+// });
 
 module.exports = app;
