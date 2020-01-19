11bad3da660e21b5b9213e2a0307ff7c0a387ad1
fairouz <fairouzkoehlerwest@gmail.com>
2020-01-19 21:27:13 +0100

    creates containers

--- client/src/containers/MainContainer.js
+++ client/src/containers/MainContainer.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import '../App.css';
 import CreateCard from '../components/CreateCard'
+import ToDoCardContainer from './ToDoCardContainer'
 class MainContainer extends Component {
   render() {
     return <div>
+      <ToDoCardContainer/>
   <CreateCard/>
     </div>;
   }
---
+++ client/src/containers/ToDoCardContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ToDoCardContainer extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default ToDoCardContainer;
